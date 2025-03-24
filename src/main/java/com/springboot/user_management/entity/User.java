a8b00fef9c6676a2640edf673e84e990e9098206
nvson <vietson6103@gmail.com>
2025-03-24 15:57:49 +0700

    mini change

--- src/main/java/com/springboot/user_management/entity/User.java
+++ src/main/java/com/springboot/user_management/entity/User.java
@@ -12,7 +12,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
